761bbc18068210d3d58ccdd8f3f749e976a4f246
Jabi <michal.jabi@gmail.com>
2024-07-09 15:54:26 +0200

    explain(02 AP): TheHeader with defineProps.

--- auction-portal/src/App.vue
+++ auction-portal/src/App.vue
@@ -1,9 +1,12 @@
 <script setup>
 import TheHeader from '@/components/TheHeader.vue'
+
+// const sth = 1_000_000
 </script>
 
 <template>
   <TheHeader />
+
   <!-- <RouterView /> -->
 </template>
 
--- auction-portal/src/components/TheHeader.vue
+++ auction-portal/src/components/TheHeader.vue
@@ -1,9 +1,14 @@
 <script setup>
-
+defineProps({
+  title: {
+    type: String,
+    default: 'Auction Portal'
+  }
+})
 </script>
 
 <template>
-    <header class="p-5 mb-4 bg-light rounded-3">
-        <h1>Auction Portal</h1>
+  <header class="p-5 mb-4 bg-light rounded-3">
+    <h1>{{ title }}</h1>
   </header>
 </template>
