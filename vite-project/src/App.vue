bdb85f3d11b06a8fd0cc75201f1d8dcc74c20d97
Jabi <michal.jabi@gmail.com>
2024-07-09 16:04:42 +0200

    feat: vite-project scaffold (without vue-create) just npm create vite@latest + vue with js.

---
+++ vite-project/src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import HelloWorld from "./components/HelloWorld.vue";
+</script>
+
+<template>
+  <HelloWorld msg="Vite + Vue" />
+  <HelloWorld />
+  <HelloWorld />
+  <HelloWorld msg="Vite + Vue 3" />
+</template>
---
+++ vite-project/src/components/MyComponent.vue
@@ -0,0 +1,11 @@
+<template>
+    <div >
+        <div v-for="item in items" :key="item.id">
+            {{ item }}
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
