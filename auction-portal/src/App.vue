db1cb97643d2199afda266cba3478500883fee33
Jabi <michal.jabi@gmail.com>
2024-07-09 16:00:03 +0200

    task(03 AP): TheMainMenu - initial - template.

--- auction-portal/src/App.vue
+++ auction-portal/src/App.vue
@@ -1,13 +1,16 @@
 <script setup>
 import TheHeader from '@/components/TheHeader.vue'
+import TheMainMenu from '@/components/TheMainMenu.vue'
 
 // const sth = 1_000_000
 </script>
 
 <template>
   <TheHeader />
-
-  <!-- <RouterView /> -->
+  <main class="container">
+    <TheMainMenu />
+    <!-- <RouterView /> -->
+  </main>
 </template>
 
 <style scoped></style>
---
+++ auction-portal/src/components/TheMainMenu.vue
@@ -0,0 +1,22 @@
+<script setup></script>
+
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <button class="navbar-toggler" type="button">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse show">
+      <ul class="navbar-nav">
+        <li class="nav-item active">
+          <a class="nav-link" href="auctions">Aukcje</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="promotions">Promocje</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="advices">Podpowiadamy</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
