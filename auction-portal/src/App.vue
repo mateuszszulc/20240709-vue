f0ca29dbae574ecaf1de9d8d2e3388070f636256
Jabi <michal.jabi@gmail.com>
2024-07-09 15:38:47 +0200

    task(02 AP): TheHeader component

--- auction-portal/src/App.vue
+++ auction-portal/src/App.vue
@@ -1,26 +1,10 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import TheHeader from '@/components/TheHeader.vue'
 </script>
 
 <template>
-  <header class="p-5 mb-4 bg-light rounded-3">
-    <h1>Auction Portal ?</h1>
-  </header>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <TheHeader />
+  <!-- <RouterView /> -->
 </template>
 
 <style scoped></style>
---
+++ auction-portal/src/components/TheHeader.vue
@@ -0,0 +1,9 @@
+<script setup>
+
+</script>
+
+<template>
+    <header class="p-5 mb-4 bg-light rounded-3">
+        <h1>Auction Portal</h1>
+  </header>
+</template>
