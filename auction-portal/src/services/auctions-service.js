bff3460fac85183af9d95a4e86f18b63f91a45c1
Jabi <michal.jabi@gmail.com>
2024-07-10 15:59:27 +0200

    explain: using .env files and environment variables

--- auction-portal/src/services/auctions-service.js
+++ auction-portal/src/services/auctions-service.js
@@ -2,7 +2,7 @@
 import axios from 'axios'
 
 const client = axios.create({
-    baseURL: 'http://localhost:3000'
+    baseURL: import.meta.env.VITE_BACKEND_URL
 })
 
 export const auctionsService = {
