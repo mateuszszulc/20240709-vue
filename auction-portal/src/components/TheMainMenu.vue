fb41668eac1647283ca86676ed7f18b7d05512e6
Jabi <michal.jabi@gmail.com>
2024-07-10 09:44:01 +0200

    task(d2-02.1): menuItems as data model

--- auction-portal/src/components/TheMainMenu.vue
+++ auction-portal/src/components/TheMainMenu.vue
@@ -1,4 +1,10 @@
-<script setup></script>
+<script setup>
+const menuItems = [
+  { href: '/auctions', title: 'Aukcje' },
+  { href: '/promotions', title: 'Promocje' },
+  { href: '/advices', title: 'Podpowiadamy' }
+]
+</script>
 
 <template>
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
@@ -7,14 +13,8 @@
     </button>
     <div class="collapse navbar-collapse show">
       <ul class="navbar-nav">
-        <li class="nav-item active">
-          <a class="nav-link" href="auctions">Aukcje</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="promotions">Promocje</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="advices">Podpowiadamy</a>
+        <li class="nav-item" v-for="item of menuItems" :key="item.href">
+          <a class="nav-link" :href="item.href">{{ item.title }}</a>
         </li>
       </ul>
     </div>
