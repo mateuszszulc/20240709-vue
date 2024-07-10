8a844a903610a75bff22d3aaf2a8014bb7659f41
Jabi <michal.jabi@gmail.com>
2024-07-10 15:47:13 +0200

    explain(living-theory): use can mix setup with data

--- vue-living-theory-project/src/views/8-mixins/components/SkywalkerGender.vue
+++ vue-living-theory-project/src/views/8-mixins/components/SkywalkerGender.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    {{ hello }}
     <span v-if="loading">Loading Gender...</span>
     <span v-else-if="errorMessage"> Błąd ładowania Gender 😭</span>
     <span v-else>Gender: {{ skywalker.gender }}</span>
@@ -12,5 +13,11 @@ import { skywalkerPreloaderMixin } from "../skywalkerLoaderMixin";
 export default {
   name: "SkywalkerGender",
   mixins: [skywalkerPreloaderMixin],
+  setup() {
+    return { hello: "test" };
+  },
+  data() {
+    return { hello2: "test" };
+  },
 };
 </script>
