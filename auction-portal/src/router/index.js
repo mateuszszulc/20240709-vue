c4956c41cd9a1452243b715acf4723100b32cae4
Jabi <michal.jabi@gmail.com>
2024-07-10 11:15:48 +0200

    task(d2-05): add missing routes to all Views

--- auction-portal/src/router/index.js
+++ auction-portal/src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import AuctionsView from '../views/AuctionsView.vue'
+import PromotionsView from '../views/PromotionsView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -16,10 +17,15 @@ const router = createRouter({
     {
       path: '/promotions',
       name: 'promotions',
+      component: PromotionsView
+    },
+    {
+      path: '/advices',
+      name: 'advices',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      component: () => import('../views/AdvicesView.vue')
     }
   ]
 })
---
+++ auction-portal/src/views/AdvicesView.vue
@@ -0,0 +1,5 @@
+<script setup></script>
+
+<template>
+  <h2>Blog: doradzamy...</h2>
+</template>
