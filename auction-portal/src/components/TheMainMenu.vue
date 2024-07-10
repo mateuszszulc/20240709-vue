ca68bd26d52589ba6cea06c000a74d0f6140de59
Jabi <michal.jabi@gmail.com>
2024-07-10 10:00:32 +0200

    task(d2-02.2): add menuToggler

--- auction-portal/src/components/TheMainMenu.vue
+++ auction-portal/src/components/TheMainMenu.vue
@@ -1,17 +1,24 @@
 <script setup>
+import { ref } from 'vue'
+
+const isMenuOpen = ref(false)
 const menuItems = [
   { href: '/auctions', title: 'Aukcje' },
   { href: '/promotions', title: 'Promocje' },
   { href: '/advices', title: 'Podpowiadamy' }
 ]
+
+function toggleMenu() {
+  isMenuOpen.value = !isMenuOpen.value
+}
 </script>
 
 <template>
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <button class="navbar-toggler" type="button">
+    <button class="navbar-toggler" type="button" @click="toggleMenu()" >
       <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse show">
+    <div class="collapse navbar-collapse" :class="{ show: isMenuOpen }">
       <ul class="navbar-nav">
         <li class="nav-item" v-for="item of menuItems" :key="item.href">
           <a class="nav-link" :href="item.href">{{ item.title }}</a>
