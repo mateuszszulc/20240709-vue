db66d7a33d0313652dd99110b073ce193773faea
Jabi <michal.jabi@gmail.com>
2024-07-11 10:28:54 +0200

    refactor(d3-03): icons - make it right - part 2 (add library of icons)

--- auction-portal/src/components/auctions/AuctionCard.vue
+++ auction-portal/src/components/auctions/AuctionCard.vue
@@ -1,5 +1,4 @@
 <script setup>
-import { faCartPlus } from '@fortawesome/free-solid-svg-icons'
 import { string, number, shape } from 'vue-types'
 defineProps({
   auction: shape({
@@ -32,7 +31,7 @@ defineEmits(['addToCart'])
       <div class="d-flex justify-content-between align-content-center">
         <strong> {{ auction.price }} zł</strong>
         <button class="btn btn-primary" @click="$emit('addToCart', auction)">
-          <fa-icon :icon="faCartPlus" />
+          <fa-icon icon="fa-cart-plus" />
         </button>
       </div>
     </div>
--- auction-portal/src/main.js
+++ auction-portal/src/main.js
@@ -1,5 +1,12 @@
 import 'bootstrap/dist/css/bootstrap.css'
+
+// icons:
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faCartPlus, faGavel } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faCartPlus, faGavel)
+// end icons;
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
