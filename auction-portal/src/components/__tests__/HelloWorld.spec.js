a30b8f7b5688b3740aa4aa717713d770adb4f856
Jabi <michal.jabi@gmail.com>
2024-07-11 15:21:54 +0200

    explain: how to test

--- auction-portal/src/components/__tests__/HelloWorld.spec.js
+++ auction-portal/src/components/__tests__/HelloWorld.spec.js
@@ -1,4 +1,4 @@
-import { describe, it, expect } from 'vitest'
+// import { describe, it, expect } from 'vitest'
 
 import { mount } from '@vue/test-utils'
 import HelloWorld from '../HelloWorld.vue'
@@ -7,5 +7,16 @@ describe('HelloWorld', () => {
   it('renders properly', () => {
     const wrapper = mount(HelloWorld, { props: { msg: 'Hello Vitest' } })
     expect(wrapper.text()).toContain('Hello Vitest')
+  })
+
+  it('should render Come on!', () => {
+    const wrapper = mount(HelloWorld, { props: { msg: 'Come on!' } })
+
+    console.log(wrapper.text());
+
+    expect(wrapper.text()).toContain('Come on!')
+    // spodziewamy sie w h1 tego tekstu:
+    expect(wrapper.get('h1.green').text()).toContain('Come on!')
+    expect(wrapper.get('[data-testid="msg"]').text()).toContain('Come on!')
   })
 })
